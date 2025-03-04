8ed4c5b4862b7bdd331391e2130c58a3e784f6f8
Carolina García <148311509+Carito0323@users.noreply.github.com>
2025-03-04 13:58:15 -0500

    Add files via upload

---
+++ Boton.js
@@ -0,0 +1,21 @@
+$('#btn-viernes').click(function() {
+    Swal.fire({
+        title: "Perfecto!",
+        text: "Mándame esta info al WhatsApp",
+        imageUrl: "https://themouseminute.wordpress.com/wp-content/uploads/2021/04/4689bcbe-9276-4f0c-9324-71fbfc6e2536.jpeg",
+        imageWidth: 300,
+        imageHeight: 400,
+        imageAlt: "Custom image"
+      })
+});
+
+$('#btn-sabado').click(function() {
+    Swal.fire({
+        title: "Perfecto!",
+        text: "Mándame esta info al WhatsApp",
+        imageUrl: "https://akamai.sscdn.co/uploadfile/letras/fotos/b/b/c/1/bbc12de6f692c1c9fd655f3a377f2c71.jpg",
+        imageWidth: 400,
+        imageHeight: 350,
+        imageAlt: "Custom image"
+      })
+});
